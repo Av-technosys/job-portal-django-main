30e7424cf16987b700751ad6f127233cecbe192a
Saksham Singh Bisht <118253416+saksha914@users.noreply.github.com>
2024-09-19 20:13:44 +0530

    made changes

--- accounts/views.py
+++ accounts/views.py
@@ -1,55 +1,35 @@
 # views.py
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.authtoken.models import Token
-from django.contrib.auth import authenticate
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
-from django.core.exceptions import ObjectDoesNotExist
-from .serializers import UserSerializer
-from .models import User
-from constants.errors import ERROR_INVALID_CREDENTIALS, ERROR_LOGOUT_FAILED
+from .serializers import UserSerializer,LoginSerializer
+from constants.errors import ERROR_LOGOUT_FAILED
 from constants.accounts import SUCCESS_LOGOUT
 from functions.common import get_login_request_payload
 
 
 @api_view(['POST'])
 def register_user(request):
-    if request.method == 'POST':
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 @api_view(['POST'])
 def user_login(request):
-    username = get_login_request_payload(request, 'username')
-    password = get_login_request_payload(request, 'password')
+    serializer = LoginSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    token_data = serializer.authenticate_user(serializer.validated_data)
+    return Response(token_data, status=status.HTTP_200_OK)
 
-    user = None
-    if '@' in username:
-        try:
-            user = User.objects.get(email=username)
-        except ObjectDoesNotExist:
-            pass
-
-    if not user:
-        user = authenticate(username=username, password=password)
-
-    if user:
-        token, _ = Token.objects.get_or_create(user=user)
-        return Response({'token': token.key}, status=status.HTTP_200_OK)
-
-    return Response({'error': ERROR_INVALID_CREDENTIALS}, status=status.HTTP_401_UNAUTHORIZED)
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
-def user_logout(request):
-    if request.method == 'POST':
-        try:
-            # Delete the user's token to logout
-            request.user.auth_token.delete()
-            return Response({'message': SUCCESS_LOGOUT}, status=status.HTTP_200_OK)
-        except Exception as e:
-            return Response({'error': ERROR_LOGOUT_FAILED}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+def user_logout(request): 
+    try:
+        request.user.auth_token.delete()
+        return Response({'message': SUCCESS_LOGOUT}, status=status.HTTP_200_OK)
+    except Exception as e:
+        return Response({'error': ERROR_LOGOUT_FAILED}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
--- constants/errors.py
+++ constants/errors.py
@@ -1,3 +1,4 @@
 ERROR_INVALID_CREDENTIALS = 'Invalid credentials'
 ERROR_USER_NOT_FOUND = 'User not found'
 ERROR_LOGOUT_FAILED = 'Failed to log out. Please try again later.'
+EMAIL_VALIDATE_ERROR='Invalid email format.'
