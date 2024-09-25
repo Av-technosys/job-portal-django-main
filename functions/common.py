3e07b664515d1b179598fb42fb81b575f91a8cd6
Dilip Kumar <dilipkumar1.edu@gmail.com>
2024-09-25 22:37:21 +0530

    endpoint for delete

--- functions/common.py
+++ functions/common.py
@@ -6,7 +6,8 @@ from constants.errors import *
 def get_login_request_payload(request: Request, key: str, default=None):
     return request.data.get(key, default)
 
-def handle_serializer(serializer):
+def serializer_handle(Serializers, request):
+    serializer = Serializers(data=request.data)
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -24,3 +25,27 @@ def update_handle(model_class, serializer_class, request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_200_OK)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+def get_handle_profile(model, serializer_class, request):
+    try:
+        instance = model.objects.get(user=request.user)
+        serializer = serializer_class(instance)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    except model.DoesNotExist:
+        return Response({"error": ERROR_NOT_FOUND}, status=status.HTTP_404_NOT_FOUND)
+
+
+def get_handle(model, serializer_class, request):
+    request.user = 2
+    instances = model.objects.filter(user=request.user)
+    serializer = serializer_class(instances, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+def delete_handle(model, request):
+    instance_id = request.data.get('id')
+    instances = model.objects.filter(id=instance_id)
+    if instances.exists():
+        instances.delete()
+        return Response({"message" : "data removed"}, status=status.HTTP_204_NO_CONTENT)
+    return Response({"error": ERROR_NOT_FOUND}, status=status.HTTP_404_NOT_FOUND)
--- user_profiles/views.py
+++ user_profiles/views.py
@@ -1,122 +1,115 @@
-from rest_framework import status
-from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
-from functions.common import handle_serializer, update_handle
-from constants.errors import *
+from functions.common import *
 from .serializers import *
 
 
-@api_view(['GET', 'POST', 'PATCH'])
+@api_view(['GET', 'POST', 'PATCH', 'DELETE'])
 # @permission_classes([IsAuthenticated])
 def student_profile(request):
     if request.method == 'GET':
-        try:
-            profile = StudentProfile.objects.get(user=request.user)
-        except StudentProfile.DoesNotExist:
-            return Response({"error": ERROR_USER_NOT_FOUND}, status=status.HTTP_404_NOT_FOUND)
-        serializer = StudentProfileSerializer(profile)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return get_handle_profile(StudentProfile, StudentProfileSerializer, request)
     
     elif request.method == 'POST':
-        serializer = StudentProfileSerializer(data=request.data)
-        return handle_serializer(serializer)
+        return serializer_handle(StudentProfileSerializer, request)
     
     elif request.method == 'PATCH':
         return update_handle(StudentProfile, StudentProfileSerializer, request)
 
+    elif request.method == 'DELETE':
+        return delete_handle(StudentProfile, request)
 
-@api_view(['GET', 'POST', 'PATCH'])
+
+@api_view(['GET', 'POST', 'PATCH', 'DELETE'])
 # @permission_classes([IsAuthenticated])
 def academic_qualification(request):
     if request.method == 'GET':
-        qualification = AcademicQualification.objects.filter(user=request.user)
-        serializer = AcademicQualificationSerializer(qualification, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return get_handle(AcademicQualification, AcademicQualificationSerializer, request)
     
     elif request.method == 'POST':
-        serializer = AcademicQualificationSerializer(data=request.data)
-        return handle_serializer(serializer)
+        return serializer_handle(AcademicQualificationSerializer, request)
 
     elif request.method == 'PATCH':
         return update_handle(AcademicQualification, AcademicQualificationSerializer, request)
 
+    elif request.method == 'DELETE':
+        return delete_handle(AcademicQualification, request)
 
-@api_view(['GET', 'POST', 'PATCH'])
+
+@api_view(['GET', 'POST', 'PATCH', 'DELETE'])
 # @permission_classes([IsAuthenticated])
 def work_experience(request):
     if request.method == 'GET':
-        experiences = WorkExperience.objects.filter(user=request.user)
-        serializer = WorkExperienceSerializer(experiences, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return get_handle(WorkExperience, WorkExperienceSerializer, request)
     
     elif request.method == 'POST':
-        serializer = WorkExperienceSerializer(data=request.data)
-        return handle_serializer(serializer)
+        return serializer_handle(WorkExperienceSerializer, request)
 
     elif request.method == 'PATCH':
         return update_handle(WorkExperience, WorkExperienceSerializer, request)
 
+    elif request.method == 'DELETE':
+        return delete_handle(WorkExperience, request)
 
-@api_view(['GET', 'POST', 'PATCH'])
+
+@api_view(['GET', 'POST', 'PATCH', 'DELETE'])
 # @permission_classes([IsAuthenticated])
 def skill_set(request):
     if request.method == 'GET':
-        skills = SkillSet.objects.filter(user=request.user)
-        serializer = SkillSetSerializer(skills, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return get_handle(SkillSet, SkillSetSerializer, request)
 
     elif request.method == 'POST':
-        serializer = SkillSetSerializer(data=request.data)
-        return handle_serializer(serializer)
+        return serializer_handle(SkillSetSerializer, request)
 
     elif request.method == 'PATCH':
         return update_handle(SkillSet, SkillSetSerializer, request)
 
+    elif request.method == 'DELETE':
+        return delete_handle(SkillSet, request)
 
-@api_view(['GET', 'POST', 'PATCH'])
+
+@api_view(['GET', 'POST', 'PATCH', 'DELETE'])
 # @permission_classes([IsAuthenticated])
 def projects(request):
     if request.method == 'GET':
-        projects = Projects.objects.filter(user=request.user)
-        serializer = ProjectsSerializer(projects, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return get_handle(Projects, ProjectsSerializer, request)
     
     elif request.method == 'POST':
-        serializer = ProjectsSerializer(data=request.data)
-        return handle_serializer(serializer)
+        return serializer_handle(ProjectsSerializer, request)
 
     elif request.method == 'PATCH':
         return update_handle(Projects, ProjectsSerializer, request)
 
+    elif request.method == 'DELETE':
+        return delete_handle(Projects, request)
 
-@api_view(['GET', 'POST', 'PATCH'])
+
+@api_view(['GET', 'POST', 'PATCH', 'DELETE'])
 # @permission_classes([IsAuthenticated])
 def certifications(request):
     if request.method == 'GET':
-        certifications = Certifications.objects.filter(user=request.user)
-        serializer = CertificationsSerializer(certifications, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return get_handle(Certifications, CertificationsSerializer, request)
 
     elif request.method == 'POST':
-        serializer = CertificationsSerializer(data=request.data)
-        return handle_serializer(serializer)
+        return serializer_handle(CertificationsSerializer, request)
 
     elif request.method == 'PATCH':
         return update_handle(Certifications, CertificationsSerializer, request)
 
+    elif request.method == 'DELETE':
+        return delete_handle(Certifications, request)
 
-@api_view(['GET', 'POST', 'PATCH'])
+
+@api_view(['GET', 'POST', 'PATCH', 'DELETE'])
 # @permission_classes([IsAuthenticated])
 def social_urls(request):
     if request.method == 'GET':
-        urls = SocialUrls.objects.filter(user=request.user)
-        serializer = SocialUrlsSerializer(urls, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return get_handle(SocialUrls, SocialUrlsSerializer, request)
 
     elif request.method == 'POST':
-        serializer = SocialUrlsSerializer(data=request.data)
-        return handle_serializer(serializer)
+        return serializer_handle(SocialUrlsSerializer, request)
 
     elif request.method == 'PATCH':
         return update_handle(SocialUrls, SocialUrlsSerializer, request)
+
+    elif request.method == 'DELETE':
+        return delete_handle(SocialUrls, request)
