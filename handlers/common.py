787ac5241fddfb8cb559d477a2aedf248306f80c
Dilip Kumar <dilipkumar1.edu@gmail.com>
2024-10-15 23:58:32 +0530

    filter and search

--- handlers/common.py
+++ handlers/common.py
@@ -16,3 +16,22 @@ def request_handler(model, serializer, request):
             return ResponseHandler.error(
                 METHOD_ERROR, status_code=status.HTTP_405_METHOD_NOT_ALLOWED
             )
+
+def post_filter_handler(request):
+    filters = {}
+    if request.data.get('education'):
+        filters['user__academicqualification__specialization__icontains'] = request.data.get('education')
+
+    if request.data.get('location'):
+        filters['city__icontains'] = request.data.get('location')
+
+    if request.data.get('experience'):
+        filters['experience__gte'] = request.data.get('experience')
+
+    if request.data.get('skills'):
+        filters['user__skillset__skill_name__icontains'] = request.data.get('skills')
+
+    if request.data.get('salary_expectations'):
+        filters['expecting_salary__lte'] = request.data.get('salary_expectations')
+        
+    return filters
