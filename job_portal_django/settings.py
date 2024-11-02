6b42e9eec9704c98a533fc3ebc80a290c2b15198
VidhiMali33 <vidhimali33@gmail.com>
2024-11-03 03:21:41 +0530

    Fix: Cors issue

--- job_portal_django/settings.py
+++ job_portal_django/settings.py
@@ -37,7 +37,7 @@ ALLOWED_HOSTS = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "https://job-portal-next-ten.vercel.app",
-    "https://job-portal-django.vercel.app/",
+    "https://job-portal-django.vercel.app",
 ]
 
 # Application definition
